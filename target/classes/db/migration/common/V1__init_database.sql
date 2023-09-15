7f794500d3030ae95d9ffda6d1db5cc53b391ae9
TC <timur.celikel@proton.me>
2023-09-15 11:23:24 -0700

    Delete target directory
    Some target dir files were mistakenly added to version control

--- target/classes/db/migration/common/V1__init_database.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists order_header;
-create table order_header
-(
-    id            bigint not null auto_increment,
-    customer_name varchar(255),
-    primary key (id)
-) engine = InnoDB;

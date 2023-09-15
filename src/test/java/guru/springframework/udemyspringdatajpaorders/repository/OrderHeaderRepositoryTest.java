6280b8ae616a9a3bf41d547cc442f522020df4a3
Timur Celikel <timur.celikel@proton.me>
2023-09-15 11:21:11 -0700

    Added local profile

--- src/test/java/guru/springframework/udemyspringdatajpaorders/repository/OrderHeaderRepositoryTest.java
+++ src/test/java/guru/springframework/udemyspringdatajpaorders/repository/OrderHeaderRepositoryTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+@ActiveProfiles("local")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class OrderHeaderRepositoryTest {
